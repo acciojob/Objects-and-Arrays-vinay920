320a5e46ab5dda7b32d13bc60922f600a4ffc1a7
vinay920 <113963978+vinay920@users.noreply.github.com>
2023-07-09 17:40:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-const players = ["John", "Bob", "Alice", "Poppy"];
-
-const person = {
-  name: "John Doe",
-  age: 80,
-};
-
 // Existing array and object
 let players = ["John", "Mike", "Emily"];
 let person = { name: "Alice", age: 25 };
@@ -22,10 +15,3 @@ let cap1 = { ...person };
 console.log(team);  // Output: ["John", "Mike", "Emily"]
 console.log(team1); // Output: ["John", "Mike", "Emily"]
 console.log(cap1);  // Output: { name: "Alice", age: 25 }
-
-
-window.players = players;
-window.person = person;
-window.team = team;
-window.team1 = team1;
-window.cap1 = cap1;
