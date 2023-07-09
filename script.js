70a95e4cb528fc37e4a0ae4e40e704506ccf5049
vinay920 <113963978+vinay920@users.noreply.github.com>
2023-07-09 17:37:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,7 +5,24 @@ const person = {
   age: 80,
 };
 
-// Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
+// Existing array and object
+let players = ["John", "Mike", "Emily"];
+let person = { name: "Alice", age: 25 };
+
+// Create a reference to the players array
+let team = players;
+
+// Create a shallow copy of the players array
+let team1 = [...players];
+
+// Create a shallow copy of the person object
+let cap1 = { ...person };
+
+// Testing the variables
+console.log(team);  // Output: ["John", "Mike", "Emily"]
+console.log(team1); // Output: ["John", "Mike", "Emily"]
+console.log(cap1);  // Output: { name: "Alice", age: 25 }
+
 
 window.players = players;
 window.person = person;
